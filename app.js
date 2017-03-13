677cf5a8c90864aea3171e756958d1fd2d939a42
Kchalmers <kevinqchal@yahoo.com>
2017-03-13 15:16:16 -0700

    switchplayers

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var CreateGame = function() {
+    this.players = [];
+    this.current_player = 'X';
     this.gameBoardSize = $("input[name='chosen']:checked").val();
     //    below are test cases. if you comment out line above, and uncomment one of the below it
     //run based on three or five.
@@ -33,6 +35,24 @@ var CreateGame = function() {
             }
         }
     };//end of createCells
+    // Start of Create Players
+    this.createPlayers = function () {
+        var player1 = $('.player1');
+        var player2 = $('.player2');
+        players.push(player1, player2);
+    };//end of createPlayers
+
+    this.switchPlayers = function (player) {
+        if(player == 'X'){
+            player1.addClass('current_player');
+            player2.removeClass('current_player');
+            current_player == 'O';
+        }  else if (player == 'O'){
+            player2.addClass('current_player');
+            player1.removeClass('current_player');
+            current_player == 'X';
+        }
+    };//End of switchPlayers
 };//end of CreateGame
 
 function startUp() {
